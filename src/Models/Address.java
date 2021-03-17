5d85c653653c2f9f8f3f2973e23d8c433bdcb8a5
TheKillerKid <kristian.hjelm@hotmail.com>
2021-03-17 10:37:25 +0100

    added gitignore

---
+++ src/Models/Address.java
@@ -0,0 +1,43 @@
+package Models;
+
+public class Address {
+	private Long addressId;
+	private String street;
+	private int houseNo;
+	private int zipcode;
+	private String city;
+	
+	
+	public Long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getHouseNo() {
+		return houseNo;
+	}
+	public void setHouseNo(int houseNo) {
+		this.houseNo = houseNo;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+
+}
