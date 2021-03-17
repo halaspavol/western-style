a442945bfb6ec2c18d77f66c0b741bd7874714ad
apeTrackunit <ape@trackunit.com>
2021-03-17 10:08:51 +0100

    Product model

--- src/Models/Product.java
+++ src/Models/Product.java
@@ -1,5 +1,47 @@
 package Models;
 
 public class Product {
-
+	private Long id;
+	private String name;
+	private String countryOfOrigin;
+	private int minStock;
+	private Long supplierId;
+	private Long barcode;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCountryOfOrigin() {
+		return countryOfOrigin;
+	}
+	public void setCountryOfOrigin(String countryOfOrigin) {
+		this.countryOfOrigin = countryOfOrigin;
+	}
+	public int getMinStock() {
+		return minStock;
+	}
+	public void setMinStock(int minStock) {
+		this.minStock = minStock;
+	}
+	public Long getSupplierId() {
+		return supplierId;
+	}
+	public void setSupplierId(Long supplierId) {
+		this.supplierId = supplierId;
+	}
+	public Long getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(Long barcode) {
+		this.barcode = barcode;
+	}
 }
