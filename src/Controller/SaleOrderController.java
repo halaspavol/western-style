bccade65185131d2adb6ffecec16044fd7900a60
TheKillerKid <kristian.hjelm@hotmail.com>
2021-03-18 13:04:27 +0100

    Making TUI changes

--- src/Controller/SaleOrderController.java
+++ src/Controller/SaleOrderController.java
@@ -1,25 +1,37 @@
 package Controller;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 import DB.DBConnection;
+import DB.SaleOrderDB;
 import Models.Customer;
 import Models.Product;
+import Models.SaleOrder;
 
 public class SaleOrderController {
-	private DBConnection dbConnection;
+	private SaleOrderDB saleOrderDB;
+	private ProductController productController;
+	private CustomerController customerController;
 	
 	public SaleOrderController() {
-		dbConnection = DBConnection.getInstance();
+		saleOrderDB = new SaleOrderDB();
 	}
 	
-	public boolean createOrder(Long id) {
-		return true;
+	public void createOrder(SaleOrder saleOrder) throws SQLException {
+		try {
+			this.saleOrderDB.create(saleOrder);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw e;
+		}
 	}
 	
 	public Customer getCustomerByEmail(String email) {
-		return null;
+		return this.customerController.getCustomerByEmail(email);
 	}
 	
 	public Product getProduct(long barcode) {
-		return null;
+		return this.getProduct(barcode);
 	}
 }
